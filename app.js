06bafc427da6b31099ab3952920966c6c1c31de4
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-05 22:44:13 +0530

    Added env file

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-const Telegraf = require('telegraf');
+const { Telegraf } = require('telegraf')
+require('dotenv').config();
 
-const bot = new Telegraf('insert_bot_token_here');
-
-//method for invoking start command
- 
-bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
-    })
-})
-//method that displays the inline keyboard buttons 
-
-bot.hears('animals', ctx => {
-    console.log(ctx.from)
-    let animalMessage = `great, here are pictures of animals you would love`;
-    ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                        text: "dog",
-                        callback_data: 'dog'
-                    },
-                    {
-                        text: "cat",
-                        callback_data: 'cat'
-                    }
-                ],
-
-            ]
-        }
-    })
-})
-
-//method to start get the script to pulling updates for telegram 
-
-bot.launch();
+const bot = new Telegraf(process.env.BOT)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
