c9f8758dae5bb009fa0d845ee10819d83cc877f8
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-05 22:12:48 +0530

    Simple Bot

---
+++ app.js
@@ -0,0 +1,38 @@
+const Telegraf = require('telegraf');
+
+const bot = new Telegraf('insert_bot_token_here');
+
+//method for invoking start command
+ 
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+    })
+})
+//method that displays the inline keyboard buttons 
+
+bot.hears('animals', ctx => {
+    console.log(ctx.from)
+    let animalMessage = `great, here are pictures of animals you would love`;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "dog",
+                        callback_data: 'dog'
+                    },
+                    {
+                        text: "cat",
+                        callback_data: 'cat'
+                    }
+                ],
+
+            ]
+        }
+    })
+})
+
+//method to start get the script to pulling updates for telegram 
+
+bot.launch();
