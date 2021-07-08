7723ba215f972cb91242788721a77522f01b8d1e
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-08 14:37:05 +0530

    Bot Scheduler

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config();
+const schedule = require('node-schedule');
+
 
 const bot = new Telegraf(process.env.BOT)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+var chatID = 0;
+bot.start((ctx) => {
+    ctx.reply('Welcome we will now register this chat window for further communications!');
+    chatID = ctx.message.chat.id;
+})
+
+bot.help((ctx) => {
+    ctx.reply('hello, please type /start to register yourself with our services!');
+})
+
+const job = schedule.scheduleJob('42 * * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
+    bot.telegram.sendMessage(chatID, 'Send me a sticker');
+});
+
 bot.launch()
