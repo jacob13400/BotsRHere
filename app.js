c4385d1b68fa0c0e2bb368737617e5b5fcface86
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-26 19:33:19 +0530

    Schedule - UPdate

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config();
 const schedule = require('node-schedule');
-
+const rule = new schedule.RecurrenceRule();
+rule.second = 42;
 
 const bot = new Telegraf(process.env.BOT)
 var chatID = 0;
@@ -14,9 +15,12 @@ bot.help((ctx) => {
     ctx.reply('hello, please type /start to register yourself with our services!');
 })
 
-const job = schedule.scheduleJob('42 * * * * *', function(){
+const job = schedule.scheduleJob(rule, function(){
     console.log('The answer to life, the universe, and everything!');
-    bot.telegram.sendMessage(chatID, 'Send me a sticker');
+    bot.telegram.sendMessage(chatID, 'Send me a sticker if you alive');
 });
-
 bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
